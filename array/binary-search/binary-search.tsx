bbe07fee66570a60675246092b26b3127bb3d7ff
Vikash Kumar <ivk@Vikashs-MacBook-Air.local>
2026-01-31 00:21:09 +0530

    update code

--- array/binary-search/binary-search.tsx
+++ array/binary-search/binary-search.tsx
@@ -1,20 +1,19 @@
-function search(nums: number[], target: number): number {
-    let low =0;
-    let high = nums.length -1;
-    let mid;
+function searchInsert(nums: number[], target: number): number {
+    let low=0;
+    let high= nums.length-1;
+    
+    while (low <= high){
+        let mid = Math.floor((low+high)/2);
 
-    while(high >= low){
-        mid = low + Math.floor((high-low)/2);
-
-        if(nums[mid] === target){
-            return mid;
+        if(nums[mid] === target) return mid;
+        if(nums[mid] < target ){
+            if(low === high) return mid+1;
+            low = mid + 1;
         }
-        if(nums[mid] >target ){
-            high = mid-1;
-        }else{
-            low = mid +1;
+        if(nums[mid] > target ){
+            high = mid -1;
+            if(low > high) return low;
         }
     }
-    return -1;
     
 };
